09b9aa9f79cd49aed783ac6406893dda8bf62420
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-11-29 13:21:34 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 while (true) {
     console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit)) && " °F"
     if (input.temperature(TemperatureUnit.Fahrenheit) > 41) {
-        light.setPixelColor(0, light.rgb(250, 0, 0))
-        light.setPixelColor(1, light.rgb(250, 0, 0))
-        light.setPixelColor(2, light.rgb(250, 0, 0))
-        light.setPixelColor(3, light.rgb(250, 0, 0))
-        light.setPixelColor(4, light.rgb(250, 0, 0))
-        light.setPixelColor(5, light.rgb(250, 0, 0))
-        light.setPixelColor(6, light.rgb(250, 0, 0))
-        light.setPixelColor(7, light.rgb(250, 0, 0))
-        light.setPixelColor(8, light.rgb(250, 0, 0))
-        light.setPixelColor(9, light.rgb(250, 0, 0))
+        light.setPixelColor(0, light.rgb(0, 250, 0))
+        light.setPixelColor(1, light.rgb(0, 250, 0))
+        light.setPixelColor(2, light.rgb(0, 250, 0))
+        light.setPixelColor(3, light.rgb(0, 250, 0))
+        light.setPixelColor(4, light.rgb(0, 250, 0))
+        light.setPixelColor(5, light.rgb(0, 250, 0))
+        light.setPixelColor(6, light.rgb(0, 250, 0))
+        light.setPixelColor(7, light.rgb(0, 250, 0))
+        light.setPixelColor(8, light.rgb(0, 250, 0))
+        light.setPixelColor(9, light.rgb(0, 250, 0))
     } else if (input.temperature(TemperatureUnit.Fahrenheit) < 0) {
         light.setPixelColor(0, light.rgb(0, 0, 250))
         light.setPixelColor(1, light.rgb(0, 0, 250))
@@ -23,16 +23,16 @@ while (true) {
         light.setPixelColor(8, light.rgb(0, 0, 250))
         light.setPixelColor(9, light.rgb(0, 0, 250))
     } else {
-        light.setPixelColor(0, light.rgb(0, 250, 0))
-        light.setPixelColor(1, light.rgb(0, 250, 0))
-        light.setPixelColor(2, light.rgb(0, 250, 0))
-        light.setPixelColor(3, light.rgb(0, 250, 0))
-        light.setPixelColor(4, light.rgb(0, 250, 0))
-        light.setPixelColor(5, light.rgb(0, 250, 0))
-        light.setPixelColor(6, light.rgb(0, 250, 0))
-        light.setPixelColor(7, light.rgb(0, 250, 0))
-        light.setPixelColor(8, light.rgb(0, 250, 0))
-        light.setPixelColor(9, light.rgb(0, 250, 0))
+        light.setPixelColor(0, light.rgb(250, 0, 0))
+        light.setPixelColor(1, light.rgb(250, 0, 0))
+        light.setPixelColor(2, light.rgb(250, 0, 0))
+        light.setPixelColor(3, light.rgb(250, 0, 0))
+        light.setPixelColor(4, light.rgb(250, 0, 0))
+        light.setPixelColor(5, light.rgb(250, 0, 0))
+        light.setPixelColor(6, light.rgb(250, 0, 0))
+        light.setPixelColor(7, light.rgb(250, 0, 0))
+        light.setPixelColor(8, light.rgb(250, 0, 0))
+        light.setPixelColor(9, light.rgb(250, 0, 0))
     }
     
 }
