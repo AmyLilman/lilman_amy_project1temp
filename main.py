3ef07cc1c52b31f9979244f8bed280d612bac4e7
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-11-29 13:27:14 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 while True:
     print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT)) and " °F"
     if input.temperature(TemperatureUnit.FAHRENHEIT) > 41:
-        light.set_pixel_color(0, light.rgb(0, 0, 250))
-        light.set_pixel_color(1, light.rgb(0, 0, 250))
-        light.set_pixel_color(2, light.rgb(0, 0, 250))
-        light.set_pixel_color(3, light.rgb(0, 0, 250))
-        light.set_pixel_color(4, light.rgb(0, 0, 250))
-        light.set_pixel_color(5, light.rgb(0, 0, 250))
-        light.set_pixel_color(6, light.rgb(0, 0, 250))
-        light.set_pixel_color(7, light.rgb(0, 0, 250))
-        light.set_pixel_color(8, light.rgb(0, 0, 250))
-        light.set_pixel_color(9, light.rgb(0, 0, 250)) 
-    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 0:
         light.set_pixel_color(0, light.rgb(250, 0, 0))
         light.set_pixel_color(1, light.rgb(250, 0, 0))
         light.set_pixel_color(2, light.rgb(250, 0, 0))
@@ -21,7 +10,18 @@ while True:
         light.set_pixel_color(6, light.rgb(250, 0, 0))
         light.set_pixel_color(7, light.rgb(250, 0, 0))
         light.set_pixel_color(8, light.rgb(250, 0, 0))
-        light.set_pixel_color(9, light.rgb(250, 0, 0))
+        light.set_pixel_color(9, light.rgb(250, 0, 0))        
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 0:
+        light.set_pixel_color(0, light.rgb(0, 0, 250))
+        light.set_pixel_color(1, light.rgb(0, 0, 250))
+        light.set_pixel_color(2, light.rgb(0, 0, 250))
+        light.set_pixel_color(3, light.rgb(0, 0, 250))
+        light.set_pixel_color(4, light.rgb(0, 0, 250))
+        light.set_pixel_color(5, light.rgb(0, 0, 250))
+        light.set_pixel_color(6, light.rgb(0, 0, 250))
+        light.set_pixel_color(7, light.rgb(0, 0, 250))
+        light.set_pixel_color(8, light.rgb(0, 0, 250))
+        light.set_pixel_color(9, light.rgb(0, 0, 250))        
     else:
         light.set_pixel_color(0, light.rgb(0, 250, 0))
         light.set_pixel_color(1, light.rgb(0, 250, 0))
--- main.ts
+++ main.ts
@@ -1,17 +1,6 @@
 while (true) {
     console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit)) && " °F"
     if (input.temperature(TemperatureUnit.Fahrenheit) > 41) {
-        light.setPixelColor(0, light.rgb(0, 0, 250))
-        light.setPixelColor(1, light.rgb(0, 0, 250))
-        light.setPixelColor(2, light.rgb(0, 0, 250))
-        light.setPixelColor(3, light.rgb(0, 0, 250))
-        light.setPixelColor(4, light.rgb(0, 0, 250))
-        light.setPixelColor(5, light.rgb(0, 0, 250))
-        light.setPixelColor(6, light.rgb(0, 0, 250))
-        light.setPixelColor(7, light.rgb(0, 0, 250))
-        light.setPixelColor(8, light.rgb(0, 0, 250))
-        light.setPixelColor(9, light.rgb(0, 0, 250))
-    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 0) {
         light.setPixelColor(0, light.rgb(250, 0, 0))
         light.setPixelColor(1, light.rgb(250, 0, 0))
         light.setPixelColor(2, light.rgb(250, 0, 0))
@@ -22,6 +11,17 @@ while (true) {
         light.setPixelColor(7, light.rgb(250, 0, 0))
         light.setPixelColor(8, light.rgb(250, 0, 0))
         light.setPixelColor(9, light.rgb(250, 0, 0))
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 0) {
+        light.setPixelColor(0, light.rgb(0, 0, 250))
+        light.setPixelColor(1, light.rgb(0, 0, 250))
+        light.setPixelColor(2, light.rgb(0, 0, 250))
+        light.setPixelColor(3, light.rgb(0, 0, 250))
+        light.setPixelColor(4, light.rgb(0, 0, 250))
+        light.setPixelColor(5, light.rgb(0, 0, 250))
+        light.setPixelColor(6, light.rgb(0, 0, 250))
+        light.setPixelColor(7, light.rgb(0, 0, 250))
+        light.setPixelColor(8, light.rgb(0, 0, 250))
+        light.setPixelColor(9, light.rgb(0, 0, 250))
     } else {
         light.setPixelColor(0, light.rgb(0, 250, 0))
         light.setPixelColor(1, light.rgb(0, 250, 0))
